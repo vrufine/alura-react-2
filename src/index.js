f46ef6157495e5b73e5dc195b01ad89cb35a0dca
Vinicius Rufine <vsrufine@gmail.com>
2018-07-01 14:20:47 -0300

    login css

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './css/normalize.css'
 import './css/timeline.css'
+import './css/login.css'
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
